fbc9e5948b7ff4df93be27a845220d6360c49967
Rober Villa <robertovilla2102@gmail.com>
2020-03-03 19:59:13 -0300

    created action-creators about products

--- front/src/redux/reducers/index.js
+++ front/src/redux/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
 
 //importando reducers
-
+import productsReducer from './products-reducer'
 
 export default combineReducers({
-
+    productos: productsReducer
 })
