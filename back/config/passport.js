b4d7ca46957e9a6c823570e889a5d6ed0cd6426b
German Torales <56942027+GermanTorales@users.noreply.github.com>
2020-03-03 16:15:48 -0300

    Configuracion de passport

---
+++ back/config/passport.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/UserModel");
+
+passport.use(
+  new LocalStrategy({ usernameField: "emailUser" }, function(
+    email,
+    password,
+    done
+  ) {
+    User.findOne({ where: { email: email } }).then(user => {
+      if (!user) {
+        return done(null, false);
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findByPk(id).then(user => {
+    done(null, user);
+  });
+});
