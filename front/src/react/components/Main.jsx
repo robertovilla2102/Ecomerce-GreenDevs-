b58f23736a5d32c7eef76de7deeddce44db428f6
Rober Villa <robertovilla2102@gmail.com>
2020-03-04 11:56:38 -0300

    created link boostrap and component Products

--- front/src/react/components/Main.jsx
+++ front/src/react/components/Main.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import { Link, Router, Redirect } from 'react-router-dom'
+import { Link, Router, Redirect, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 
+//importando containers
+import ProductsContainer from '../containers/ProductsContainer'
+
 class Main extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <h1>Bienvenido a la nada</h1>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Bienvenido a la nada</h1>
+        <ProductsContainer />
+
+        {/* 
+        <Switch>
+          <Router path='/products' component={ProductsContainer} />
+          <Redirect from='/' to='/products' />
+        </Switch> */}
+
+      </React.Fragment>
+    )
+  }
 }
 
 export default connect(null, null)(Main) 
