376050ea8fd39d86ab59e557c925b7f2c073e6c2
Rober Villa <robertovilla2102@gmail.com>
2020-03-04 17:25:11 -0300

    arreglando navbar

--- front/src/react/components/Navbar.jsx
+++ front/src/react/components/Navbar.jsx
@@ -2,23 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
-<<<<<<< HEAD
-export default ()=>{
-  return(
+export default () => {
+  return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img src="/imagenes/Logo/logo.png" width="110" height="40" alt=""/>
+      <a className="navbar-brand" href="#">
+        <img src="/imagenes/Logo/logo.png" width="110" height="40" alt="" />
       </a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
-=======
-export default () => {
-  return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">BotánicaBinaria</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
->>>>>>> 13a4a8ca2420b590b2e878e15d4ce0390f6f3894
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -36,15 +27,9 @@ export default () => {
             <a className="nav-link" href="#">Contacto</a>
           </li>
         </ul>
-<<<<<<< HEAD
         <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-=======
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
->>>>>>> 13a4a8ca2420b590b2e878e15d4ce0390f6f3894
         </form>
       </div>
     </nav>
