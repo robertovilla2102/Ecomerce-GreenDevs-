9337ff8800b5c406d203d8b116c59bbfa04319e2
unknown <luztestai24@gmail.com>
2020-03-05 10:53:23 -0300

    view single a mitad de camino

--- front/src/react/components/Main.jsx
+++ front/src/react/components/Main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Link, Route, Redirect, Switch, Router } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React from "react";
+import { Link, Route, Redirect, Switch, Router } from "react-router-dom";
+import { connect } from "react-redux";
 
 //importando containers
-import ProductsContainer from '../containers/ProductsContainer'
-import Navbar from '../containers/NavbarContainer'
-import RegisterContainer from '../containers/RegisterContainer'
-
+import ProductsContainer from "../containers/ProductsContainer";
+import Navbar from "../containers/NavbarContainer";
+import RegisterContainer from "../containers/RegisterContainer";
+import ViewSingleContainer from "../containers/ViewSingleContainer";
 
 class Main extends React.Component {
   render() {
@@ -16,23 +16,21 @@ class Main extends React.Component {
         <h1>Bienvenido a la nada</h1>
 
         <div className="container">
-          <Link to='/register'>
+          <Link to="/register">
             <button>registarse</button>
           </Link>
         </div>
 
         <Switch>
-          <Route path='/products' component={ProductsContainer} />
+          <Route exact path="/products" component={ProductsContainer} />
+          <Router exact path="/products/:id" component={ViewSingleContainer} />
+          <Route exact path="/register" component={RegisterContainer} />
 
-          <Route path='/register' component={RegisterContainer} />
-
-          <Redirect from='/' to='/products' />
-
+          <Redirect from="/" to="/products" />
         </Switch>
-
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default connect(null, null)(Main)
+export default connect(null, null)(Main);
