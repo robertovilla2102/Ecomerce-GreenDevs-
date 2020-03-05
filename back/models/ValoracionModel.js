679a5c36faa0b3132fea94cb3fe6655406abeb44
German Torales <56942027+GermanTorales@users.noreply.github.com>
2020-03-05 08:27:34 -0300

    Cambio de nombres a los modelos de base de datos

--- back/models/ValoracionModel.js
+++ back/models/ValoracionModel.js
@@ -3,10 +3,12 @@ const db = require("../config/db/index");
 
 const Valoracion = db.define("valoracion", {
   rating: {
-    type: S.DOUBLE
+    type: S.DOUBLE,
+    defaultValue: 0.0
   },
   review: {
-    type: S.STRING
+    type: S.STRING,
+    defaultValue: ""
   }
 });
 module.exports = Valoracion;
