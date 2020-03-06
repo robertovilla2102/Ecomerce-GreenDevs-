39c75e069f12f86427c79c6f814b855235f8b999
CelesteColamarino <celestecolamarino@gmail.com>
2020-03-05 21:08:24 -0300

    About us creado, categorias rediseñadas, imagenes cargadas

--- front/src/react/components/AboutUs.jsx
+++ front/src/react/components/AboutUs.jsx
@@ -5,27 +5,16 @@ import { Link } from 'react-router-dom'
 import { styleDivProduct, styleH, styleP, styleImg } from '../css/estilosComunes'
 
 export default () => (
-  <div>
-
-    <div
-      style={{ marginTop: "10px" }}
-      className='container'
-    >
-      <h3 style={styleH}>About Us</h3>
-      <p style={{
-        textAlign: "center",
-        marginTop: '2px',
-        padding: '0 80px'
-      }}
-      >Cuando elegís tener una planta, estas eligiendo dar de tu tiempo y de tu espacio, sabemos lo que eso vale, por eso en Vivero Mario generamos un lugar donde vas a encontrar la mejor calidad en plantas, el mejor surtido de macetas, y todo lo necesario para poder cuidarlas de la manera que merecen. Si elegís plantas elegís Vida.</p>
-
-      <p style={{
-        textAlign: "center",
-        marginTop: '2px',
-        padding: '0 80px'
-      }}>This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide.</p>
+  <div  style={{marginTop: "0"}} id="carouselExampleIndicators" className="carousel slide carousel-fluid" data-ride="carousel">
+    <ol className="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img className="d-block w-100" src="/imagenes/AboutUs/4.png" alt="First slide" />
+      </div>
     </div>
-
-    <img src="https://www.viveromario.com.ar/portal/images/tira-fotos-home1.jpg" alt="pepe" className='img-fluid' />
   </div>
 )
