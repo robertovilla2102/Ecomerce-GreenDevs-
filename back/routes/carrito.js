a4786150ff26f04dae6213767a837f3feef13801
German Torales <56942027+GermanTorales@users.noreply.github.com>
2020-03-06 12:54:01 -0300

    Demo

--- back/routes/carrito.js
+++ back/routes/carrito.js
@@ -2,43 +2,42 @@ const express = require("express");
 const router = express.Router();
 const { Carrito, Producto } = require("../models/index");
 
-router.get("/:id", function (req, res, next) {
+router.get("/:id", function(req, res, next) {
   Carrito.findAll({
-    include: [{
-      model: Producto,
-    }],
-    where: { userId: req.params.id }
+    include: [
+      {
+        model: Producto
+      }
+    ],
+    where: { userId: req.user.id }
   })
     .then(carritos => res.json(carritos))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send(err)
-    })
-})
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
 
-router.post("/add/:productId", function (req, res, next) {
-  console.log(req.body);
+router.post("/add/:productId", function(req, res, next) {
   //const { cantidad, estado } = req.body;
 
   Carrito.create({
     cantidad: req.body.body.cantidad,
     productoId: req.params.productId,
-    userId: req.body.body.user
+    userId: req.user.id
   })
     .then(productos => {
-      console.log(productos);
-      res.json(productos)
+      res.json(productos);
     })
     .catch(err => res.json(err));
 });
 
-router.put("/edit/:id", function (req, res, next) {
+router.put("/edit/:id", function(req, res, next) {
   Carrito.update(req.body, { where: { id: req.params.id } })
     .then(res.sendStatus(200))
     .catch(res.sendStatus(500, "Fallo Actualizacion"));
 });
 
-router.delete("/delete/:id", function (req, res, next) {
+router.delete("/delete/:id", function(req, res, next) {
   Carrito.destroy({ where: { id: req.params.id } }).then(res.sendStatus(200));
 });
 
