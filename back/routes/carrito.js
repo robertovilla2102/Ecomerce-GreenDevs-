c6b7af1eb698f84606230d488e4050366ed9adbf
German Torales <56942027+GermanTorales@users.noreply.github.com>
2020-03-08 14:18:08 -0300

    El carrito persiste sin estar logueado

--- back/routes/carrito.js
+++ back/routes/carrito.js
@@ -1,44 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { Carrito, Producto } = require("../models/index");
+const CarritoController = require("../controllers/CarritoController");
 
-router.get("/", function(req, res, next) {
-  Carrito.findAll({
-    include: [
-      {
-        model: Producto
-      }
-    ],
-    where: { userId: req.user.id }
-  })
-    .then(carritos => res.json(carritos))
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
+router.get("/", CarritoController.buscarCarritos); //Busca los carritos de la persona logeada o deslogueada
 
-router.post("/add/:productId", function(req, res, next) {
-  Carrito.findOne({
-    where: { userId: req.user.id, productoId: req.params.productId }
-  })
-    .then(carritos => {
-      if (carritos) {
-        Carrito.update(
-          { cantidad: carritos.cantidad + req.body.body.cantidad },
-          { where: { id: carritos.id } }
-        ).then(() => res.status(201));
-      } else {
-        Carrito.create({
-          ...req.body.body,
-          userId: req.user.id,
-          productoId: req.params.productId
-        }).then(carrito => {
-          res.sendStatus(200);
-        });
-      }
-    })
-    .catch(err => res.json(err));
-});
+router.post("/add/:productId", CarritoController.agregarProducto); //Agrega un nuevo carrito a la base de datos o a la sesion si es que no esta logeado
 
 router.put("/edit/:id", function(req, res, next) {
   Carrito.update(req.body, { where: { id: req.params.id } })
