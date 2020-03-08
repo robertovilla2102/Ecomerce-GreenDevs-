ddca2226d0228ff8c86c24529a5596fa79685266
German Torales <56942027+GermanTorales@users.noreply.github.com>
2020-03-07 23:58:19 -0300

    Se suman si el usuario agrega dos veces el mismo producto

--- back/routes/carrito.js
+++ back/routes/carrito.js
@@ -18,15 +18,24 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/add/:productId", function(req, res, next) {
-  //const { cantidad, estado } = req.body;
-
-  Carrito.create({
-    cantidad: req.body.body.cantidad,
-    productoId: req.params.productId,
-    userId: req.user.id
+  Carrito.findOne({
+    where: { userId: req.user.id, productoId: req.params.productId }
   })
-    .then(productos => {
-      res.json(productos);
+    .then(carritos => {
+      if (carritos) {
+        Carrito.update(
+          { cantidad: carritos.cantidad + req.body.body.cantidad },
+          { where: { id: carritos.id } }
+        ).then(() => res.status(201));
+      } else {
+        Carrito.create({
+          ...req.body.body,
+          userId: req.user.id,
+          productoId: req.params.productId
+        }).then(carrito => {
+          res.sendStatus(200);
+        });
+      }
     })
     .catch(err => res.json(err));
 });
