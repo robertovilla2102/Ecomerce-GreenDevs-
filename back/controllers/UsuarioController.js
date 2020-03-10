cad4cf6b238a70ebaeaa0fa35134f41d1435aa6b
unknown <luztestai24@gmail.com>
2020-03-10 16:56:11 -0300

    ...

--- back/controllers/UsuarioController.js
+++ back/controllers/UsuarioController.js
@@ -30,27 +30,45 @@ UsuarioController.crearUsuario = (req, res) => {
     .catch(error => res.send(error));
 };
 
-UsuarioController.login = (req, res) => {
-  var listCarrito = req.session.carrito;
-  listCarrito.forEach(carrito => {
-    Carrito.findOne({
-      where: { userId: req.user.id, productoId: carrito.producto.id }
-    }).then(carritos => {
-      if (carritos) {
-        Carrito.update(
-          { cantidad: carritos.cantidad + carrito.cantidad },
-          { where: { id: carritos.id } }
-        );
-      } else {
-        Carrito.create({
-          estado: carrito.estado,
-          cantidad: carrito.cantidad,
-          userId: req.user.id,
-          productoId: carrito.producto.id
-        });
-      }
+UsuarioController.edit = (req, res) => {
+  User.update(req.body.user, { where: { id: req.user.id } })
+    .then(() => {
+      return User.findOne({ where: { id: req.user.id } });
+    })
+    .then(user => {
+      res.status(201).json({
+        userName: user.userName,
+        userEmail: user.userEmail,
+        userProfile: user.imgProfile,
+        userBirthDay: user.birthDay,
+        userAddress: user.address
+      });
     });
-  });
+};
+
+UsuarioController.login = async (req, res) => {
+  var listCarrito = req.session.carrito;
+  if (listCarrito) {
+    await listCarrito.forEach(carrito => {
+      Carrito.findOne({
+        where: { userId: req.user.id, productoId: carrito.producto.id }
+      }).then(carritos => {
+        if (carritos) {
+          Carrito.update(
+            { cantidad: carritos.cantidad + carrito.cantidad },
+            { where: { id: carritos.id } }
+          );
+        } else {
+          Carrito.create({
+            estado: carrito.estado,
+            cantidad: carrito.cantidad,
+            userId: req.user.id,
+            productoId: carrito.producto.id
+          });
+        }
+      });
+    });
+  }
   res.status(200).json({
     id: req.user.id,
     userName: req.user.userName,
